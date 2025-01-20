9e6147e8b738b3d27474e3b612d5d58da77217b5
Nao-ki11704 <naoki18075@gmail.com>
2025-01-20 17:14:51 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let bodyWeight;
+let Height;
+let bodyMassindex;
+
+// 値の代入
+bodyWeight = 68;
+Height = 1.7;
+bodyMassindex = 68/(1.7*1.7)
+
+// コンソールへの出力
+console.log(bodyWeight);
+console.log(Height);
+console.log(bodyMassindex);
